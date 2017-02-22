45d38ee542d6d3ebefbfacd92a8e09ae61ba4cd0
Shvein Anton <t0hashvein@gmail.com>
2017-02-22 11:27:17 +0500

    Record for element fixed and documented

--- include/records.hrl
+++ include/records.hrl
@@ -1,1 +1,21 @@
--record(sample_nitrogen_plugin, {?ELEMENT_BASE(element_sample_nitrogen_plugin), field1, field2 }).
+%% Table column definition like
+%% {MapField, "Column Title", 'input type'}
+-type column() :: {atom(), string(), input_type()}.
+
+%% Input type for column data updatting
+%% 'tb' - inplace textbox
+%% 'ta' - inplace textarea
+%% 'none' - just print stringified data
+%% {select, DropDownOptions} - select element,
+%% for DropDownOptions syntax see http://nitrogenproject.com/doc/elements/dropdown.html
+-type input_type() :: tb | ta | {select, [{string(), string()}]} | none.
+
+-record(crud, {?ELEMENT_BASE(element_crud),
+               table_class=[], % Additional classes for table element
+               button_class=[], % Additional classes for CRUD buttons (last column)
+               pagination_class=[], % Pagination buttons classes
+               cols=[] ::  [column()], % Missed columns are not displayed
+               funs=#{}, % Funs for "list", "delete" and "update" functions, see README example
+               start=0, % Pagination start for current page
+               count=10 % Pagination elements per page
+    }).
